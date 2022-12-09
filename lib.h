3b902df31faadea45ade4cdd749a829b801d5ec4
lorenzolatona <115403824+lorenzolatona@users.noreply.github.com>
2022-12-09 16:37:08 +0100

    Update lib.h

--- lib.h
+++ lib.h
@@ -2,7 +2,7 @@
 // Created by Lorenzo Latona Schettini on 09/12/2022
 //
 
-#ifndef ESERCITAZIONE8
-#define ESERCITAZIONE8
+#ifndef ESERCITAZIONE8_lib_h
+#define ESERCITAZIONE8_lib_h
 float trovaMax(float [],int);
 #endif // ESERCITAZIONE8_lib_h

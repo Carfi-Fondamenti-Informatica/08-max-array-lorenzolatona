b48ecd413ccbbdb24fe13aaa660c808b41e10cb0
lorenzolatona <115403824+lorenzolatona@users.noreply.github.com>
2022-12-09 16:36:04 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,16 @@
+//
+// Created br Lorenzo Latona Schettini on 09/12/2022
+//
 
+#include "lib.h"
+
+float trovaMax(float mat[],int n){
+  float a=mat[0];
+  
+  for(int i=1;i<n;i++){
+    if(mat[i]>a){
+      a=mat[i];
+    }
+  }
+  return a;
+}
